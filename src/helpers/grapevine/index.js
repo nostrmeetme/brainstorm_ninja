1be9805ae32e28975eecbfe9c9d6f3dc0457cce2
David Strayhorn <david.strayhorn4@gmail.com>
2024-08-12 23:52:14 -0400

    comment out debugging lines in convertInputToCertainty

--- src/helpers/grapevine/index.js
+++ src/helpers/grapevine/index.js
@@ -1,9 +1,10 @@
 export const convertInputToCertainty = (input, rigor) => {
-  console.log('convertInputToCertainty; input: ' + input + '; rigor: ' + rigor)
+  // console.log('convertInputToCertainty; input: ' + input + '; rigor: ' + rigor)
   const rigority = -Math.log(rigor)
   const fooB = -input * rigority
   const fooA = Math.exp(fooB)
   const certainty = 1 - fooA
+  /*
   console.log(
     'convertInputToCertainty; input: ' +
       input +
@@ -18,5 +19,6 @@ export const convertInputToCertainty = (input, rigor) => {
       '; certainty: ' +
       certainty,
   )
+  */
   return certainty.toPrecision(4)
 }
