22140bed412ff5b60026b082d9304d14ce46c1f3
David Strayhorn <david.strayhorn4@gmail.com>
2024-08-12 23:41:56 -0400

    debugging certainty calc

--- src/helpers/grapevine/index.js
+++ src/helpers/grapevine/index.js
@@ -1,7 +1,22 @@
 export const convertInputToCertainty = (input, rigor) => {
+  console.log('convertInputToCertainty; input: ' + input + '; rigor: ' + rigor)
   const rigority = -Math.log(rigor)
   const fooB = -input * rigority
   const fooA = Math.exp(fooB)
   const certainty = 1 - fooA
+  console.log(
+    'convertInputToCertainty; input: ' +
+      input +
+      '; rigor: ' +
+      rigor +
+      '; rigority: ' +
+      rigority +
+      '; fooB: ' +
+      fooB +
+      '; fooA: ' +
+      fooA +
+      '; certainty: ' +
+      certainty,
+  )
   return certainty.toPrecision(4)
 }
